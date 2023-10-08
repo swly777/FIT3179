e999d8362910f6ad3aaed6df3f2cdfc61c3babdd
Stephen <104710067+swly777@users.noreply.github.com>
2023-10-08 22:54:15 +1100

    Add files via upload

--- load.js
+++ load.js
@@ -1,6 +1,13 @@
 var vg_1 = "sample.json";
 
-vegaEmbed("#bar_chart", vg_1).then(function(result) {
+vegaEmbed("#bubble_chart", vg_1).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "sample2.json";
+
+vegaEmbed("#bar_chart", vg_2).then(function(result) {
 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
