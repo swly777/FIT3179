0d68c45a3bab394b1de24f20f5717cb190c2ef85
Stephen <104710067+swly777@users.noreply.github.com>
2023-10-08 22:33:28 +1100

    Add files via upload

---
+++ load.js
@@ -0,0 +1,6 @@
+var vg_1 = "sample.json";
+
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
